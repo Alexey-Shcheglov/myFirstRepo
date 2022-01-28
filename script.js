d4ba8e279875559e2fa501d90608ece1237ef239
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-01-28 11:28:25 +0400

    Выполнены задания второго урока

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'lesson02';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 500;
+let rollback = 38;
+let fullPrice = 1000;
+let adaptive = true;
 
 alert('Первый урок!');
-
-
 console.log('Привет консоль!');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + '$');
+console.log('Стоимость разработки сайта ' + fullPrice + '$');
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback/100));
+
