e6c6a6a30c50b093d3bcb56fd0c59668e7e0067b
Alexey Shcheglov <aleexxx2612@gmail.com>
2022-02-02 22:24:09 +0400

    Выполнено задание четвертого урока

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ let title = prompt('Как называется Ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
+
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
+
 let rollback = 10;
+let allServicePrices;
 let fullPrice;
 let servicePercentPrice = Math.round(fullPrice - (fullPrice * (rollback / 100)));
-let allServicePrices;
+
 
 
 
